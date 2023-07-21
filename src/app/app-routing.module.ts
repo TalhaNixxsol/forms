1ca39169071900d8ad3d220602376237910c412c
TalhaNixxsol <Talha.saeed@nixxsol.com>
2023-07-21 17:23:20 +0500

    loginform component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginformComponent } from './forms/loginform/loginform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginformComponent
+  },
+  {
+    path: '',
+    component: LoginformComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
