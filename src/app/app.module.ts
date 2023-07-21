d259c6b6e13912a5508444ba9a89c50d9387b62b
TalhaNixxsol <Talha.saeed@nixxsol.com>
2023-07-21 18:22:01 +0500

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginformComponent } from './forms/loginform/loginform.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { LoginformComponent } from './forms/loginform/loginform.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forms/loginform/loginform.component.ts
+++ src/app/forms/loginform/loginform.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
-  styleUrls: ['./loginform.component.scss']
+  styleUrls: ['./loginform.component.scss'],
 })
-export class LoginformComponent {
+export class LoginformComponent implements OnInit {
+  loginform!: FormGroup; // Declare 'loginform' as FormGroup
 
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Initialize the 'loginform' FormGroup and define the form controls
+    this.loginform = this.fb.group({
+      username: ['', Validators.required], // Add Validators if needed (e.g., required)
+      password: ['', Validators.required], // Add Validators if needed (e.g., required)
+    });
+  }
+
+  onSubmit(data: any) {
+    // Check if the form is valid before proceeding
+    if (this.loginform.valid) {
+      console.log(data); // 'data' already contains the form values due to form binding
+      // Proceed with further actions or form submission
+    } else {
+      console.log('Form is invalid. Please fill in all required fields.');
+    }
+  }
 }
